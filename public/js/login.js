cb7bb66bf957aa7382857f140fbcf74eb4b25389
ChrisReutter <estaverano3@gmail.com>
2020-09-30 15:35:09 +0200

    first init

---
+++ public/js/login.js
@@ -0,0 +1,45 @@
+/* eslint-disable */
+import axios from 'axios';
+import {
+  showAlert
+} from './alerts';
+
+export const login = async (email, password) => {
+  try {
+    const res = await axios({
+      method: 'POST',
+      url: 'http://localhost:3000/api/v1/users/login',
+      data: {
+        email,
+        password
+      }
+    });
+
+    if (res.data.status === 'success') {
+      showAlert('success', 'Logged In Successfully!');
+      window.setTimeout(() => {
+        location.assign('/');
+      }, 1500);
+    }
+  } catch (err) {
+    showAlert('error', err.response.data.message);
+  }
+};
+
+export const logout = async () => {
+  try {
+    const res = await axios({
+      method: 'GET',
+      url: 'http://localhost:3000/api/v1/users/logout'
+    });
+    if ((res.data.status = 'success')) location.reload(true); {
+      showAlert('error', 'Logged Out Successfully!');
+      window.setTimeout(() => {
+        location.assign('/');
+      }, 1500);
+    }
+  } catch (err) {
+    console.log(err.response);
+    showAlert('error', 'Error logging out! Try again.');
+  }
+};
