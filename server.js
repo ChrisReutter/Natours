e441814eea29b095291d6baa9a590fe06bab0eed
ChrisReutter <estaverano3@gmail.com>
2020-10-01 15:00:12 +0200

    Updated Routes

--- server.js
+++ server.js
@@ -1,3 +1,50 @@
+// const mongoose = require('mongoose');
+// const dotenv = require('dotenv');
+
+// process.on('uncaughtException', err => {
+//   console.log('UNCAUGHT EXCEPTION! 💥 Shutting down...');
+//   console.log(err.name, err.message);
+//   process.exit(1);
+// });
+
+// dotenv.config({
+//   path: './config.env'
+// });
+// const app = require('./app');
+
+// const DB = process.env.DATABASE.replace(
+//   '<PASSWORD>',
+//   process.env.DATABASE_PASSWORD
+// );
+
+// mongoose
+//   .connect(DB, {
+//     useNewUrlParser: true,
+//     useCreateIndex: true,
+//     useFindAndModify: false,
+//     useUnifiedTopology: true
+//   })
+//   .then(() => console.log('DB connection successful!'));
+
+// const port = process.env.PORT || 3000;
+// const server = app.listen(port, () => {
+//   console.log(`App running on PORT ${port}...`);
+// });
+
+// process.on('unhandledRejection', err => {
+//   console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+//   console.log(err.name, err.message);
+//   server.close(() => {
+//     process.exit(1);
+//   });
+// });
+
+// process.on('SIGTERM', () => {
+//   console.log('SIGTERM RECEIVED. Shutting down now!');
+//   server.close(() => {
+//     console.log('Process terminated');
+//   })
+// })
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
@@ -21,14 +68,13 @@ mongoose
   .connect(DB, {
     useNewUrlParser: true,
     useCreateIndex: true,
-    useFindAndModify: false,
-    useUnifiedTopology: true
+    useFindAndModify: false
   })
   .then(() => console.log('DB connection successful!'));
 
-const PORT = process.env.PORT || 3000;
-const server = app.listen(PORT, () => {
-  console.log(`App running on PORT ${PORT}...`);
+const port = process.env.PORT || 3000;
+const server = app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
 });
 
 process.on('unhandledRejection', err => {
@@ -40,8 +86,8 @@ process.on('unhandledRejection', err => {
 });
 
 process.on('SIGTERM', () => {
-  console.log('SIGTERM RECEIVED. Shutting down now!');
+  console.log('👋 SIGTERM RECEIVED. Shutting down gracefully');
   server.close(() => {
-    console.log('Process terminated');
-  })
-})
+    console.log('💥 Process terminated!');
+  });
+});
